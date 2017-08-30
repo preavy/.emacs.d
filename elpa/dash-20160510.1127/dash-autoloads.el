969cd1ee51f475df2c79ea8d8b433ce40b1c7407
Peter Reavy <p.reavy@qub.ac.uk>
2017-08-30 15:58:28 +0100

    Don't store Elpa packages.

--- elpa/dash-20160510.1127/dash-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/dash-20160510.1127/dash-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/dash-20160510.1127/dash.el") (22343
-;;;;;;  3890 899431 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
--- elpa/with-editor-20160408.201/with-editor-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; with-editor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/with-editor-20160408.201/with-editor-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/with-editor-20160408.201/with-editor-pkg.el"
-;;;;;;  "../../../../.emacs.d/elpa/with-editor-20160408.201/with-editor.el")
-;;;;;;  (22343 3891 664333 800000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; with-editor-autoloads.el ends here
